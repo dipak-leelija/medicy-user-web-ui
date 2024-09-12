d01d32331469ad12aa52d4e8503b2aeea0ebe7f4
Jayanta <jbleelija@gmail.com>
2024-09-12 15:23:32 +0530

    update viewpurchase and ViewPurchaseReturn page'

--- src/pages/pharmacy/purchases/purchase-return/viewPurchaseReturn.tsx
+++ src/pages/pharmacy/purchases/purchase-return/viewPurchaseReturn.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../redux/store";
 import { fetchPurchaseItemsRequest, fetchPurchaseDistributorRequest } from "../../../../redux/DataFetch/actions";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Table from "../../../../components/Table";
 import { Card, Col,  Row } from "react-bootstrap";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -12,6 +12,7 @@ import SalesOffcanvas from "../../SalesOffcanvas";
 
 export default function ViewPurchase() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
   // Remove the colon (:) from the id
   const cleanedId = id?.replace(':', '');
@@ -152,10 +153,10 @@ export default function ViewPurchase() {
                     <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent' name='billId' value={filterDistributor?.payment_mode || ''} readOnly/>
                   </FloatingLabel>
                 </Col>
-                <Col>
+                <Col className="CustomTableRow px-4" onClick={()=>navigate('/distributor')}>
                 <i className="fas fa-user ms-n2" style={{ position: 'absolute', marginTop: '8px',marginRight:'160px' }}></i>
-                  <FloatingLabel className='invlabel border-0' controlId="floatingDistBillNo" label="Distributor">
-                    <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent' name='billId' value={filterDistributor?.name || ''} readOnly/>
+                  <FloatingLabel className='invlabel border-0 CustomTableRow' controlId="floatingDistBillNo" label="Distributor">
+                    <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent CustomTableRow' name='billId' value={filterDistributor?.name || ''} readOnly/>
                   </FloatingLabel>
                 </Col>
               </Row>
@@ -163,7 +164,7 @@ export default function ViewPurchase() {
                 columns={columns}
                 data={filteredPurchaseData}
                 // pageSize={10}
-                sizePerPageList={sizePerPageList}
+                // sizePerPageList={sizePerPageList}
                 isSortable={true}
                 pagination={true}
                 isSelectable={true}
--- src/pages/pharmacy/purchases/purchase/ViewPurchase.tsx
+++ src/pages/pharmacy/purchases/purchase/ViewPurchase.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../redux/store";
 import { fetchPurchaseItemsRequest, fetchPurchaseDistributorRequest } from "../../../../redux/DataFetch/actions";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Table from "../../../../components/Table";
 import { Card, Col,  Row } from "react-bootstrap";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -12,6 +12,7 @@ import SalesOffcanvas from "../../SalesOffcanvas";
 
 export default function ViewPurchase() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
   // Remove the colon (:) from the id
   const cleanedId = id?.replace(':', '');
@@ -116,20 +117,20 @@ export default function ViewPurchase() {
   ];
 
   // get pagelist to display
-  const sizePerPageList = [
-    {
-      text: "10",
-      value: 10,
-    },
-    {
-      text: "25",
-      value: 25,
-    },
-    {
-      text: "All",
-      value: filteredPurchaseData.length,
-    },
-  ];
+  // const sizePerPageList = [
+  //   {
+  //     text: "10",
+  //     value: 10,
+  //   },
+  //   {
+  //     text: "25",
+  //     value: 25,
+  //   },
+  //   {
+  //     text: "All",
+  //     value: filteredPurchaseData.length,
+  //   },
+  // ];
 
   return (
     <>
@@ -153,18 +154,18 @@ export default function ViewPurchase() {
                     <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent' name='billId' value={filterDistributor?.payment_mode || ''} readOnly/>
                   </FloatingLabel>
                 </Col>
-                <Col>
+                <Col className="CustomTableRow px-4 cursor-pointer" onClick={()=>navigate('/distributor')}>
                 <i className="fas fa-user ms-n2" style={{ position: 'absolute', marginTop: '8px',marginRight:'160px' }}></i>
-                  <FloatingLabel className='invlabel border-0' controlId="floatingDistBillNo" label="Distributor">
-                    <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent' name='billId' value={filterDistributor?.name || ''} readOnly/>
+                  <FloatingLabel className='invlabel border-0 CustomTableRow' controlId="floatingDistBillNo" label="Distributor">
+                    <Form.Control type="text" placeholder="" className='border-0 px-0 ps-1 fs-5 fw-bold bg-transparent CustomTableRow' name='billId' value={filterDistributor?.name || ''} readOnly/>
                   </FloatingLabel>
                 </Col>
               </Row>
               <Table
                 columns={columns}
                 data={filteredPurchaseData}
-                pageSize={10}
-                sizePerPageList={sizePerPageList}
+                // pageSize={10}
+                // sizePerPageList={sizePerPageList}
                 isSortable={true}
                 pagination={true}
                 isSelectable={true}
