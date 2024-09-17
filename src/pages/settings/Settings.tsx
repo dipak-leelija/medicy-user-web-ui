234fe0c29797591c1afd69ccdbab63394e1bb3a6
pritam-parui <pritamparui.leelija@gmail.com>
2024-09-17 18:29:00 +0530

    working on pdf prev

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import img from '../../assets/images/companies/airbnb.png';
 import Swal from 'sweetalert2';
 import { Button, Card, Col, Form, FormControl, Row, Tab, Tabs, Toast, ToastContainer, ToastHeader, ToastBody } from 'react-bootstrap';
+
 
 export default function Settings() {
 
@@ -25,7 +26,7 @@ export default function Settings() {
                 if (result.isConfirmed) {
                     const fileName = file.name;
                     const extFile = fileName.split('.').pop()?.toLowerCase();
-                    if (extFile === "jpg" || extFile === "jpeg" || extFile === "png") {
+                    if (extFile === 'pdf' || extFile === "jpg" || extFile === "jpeg" || extFile === "png") {
                         readURL(file);
                     } else {
                         if (errorRef.current) errorRef.current.classList.remove("d-none");
@@ -45,6 +46,30 @@ export default function Settings() {
         reader.readAsDataURL(file);
     };
 
+    const [form20Preview, setForm20Preview] = useState<string | null>(null);
+    const [form21Preview, setForm21Preview] = useState<string | null>(null);
+
+    const form20Ref = useRef<HTMLInputElement | null>(null);
+    const form21Ref = useRef<HTMLInputElement | null>(null);
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>, type: 'form20' | 'form21') => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            if (reader.result && typeof reader.result === 'string') {
+                if (type === 'form20') {
+                    setForm20Preview(reader.result);
+                } else if (type === 'form21') {
+                    setForm21Preview(reader.result);
+                }
+            }
+        };
+
+        reader.readAsDataURL(file);
+    };
 
 
 
@@ -65,11 +90,18 @@ export default function Settings() {
         Country: '',
     });
     // Control toast visibility
+    const [DrugInputs, setDrugInputs] = useState({
+        GSTnumber: '',
+        PANnumber: '',
+
+    })
     const [showToast, setShowToast] = useState(false);
     const [validated, setValidated] = useState(false);
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const contactRegex = /^\d{10}$/;
+    const GstNumberRegex = /^\d{15}$/;
+    const PANnumberRegex = /^\d{10}$/;
     const pinRegex = /^\d{6}$/;
 
     const handleFromChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement>) => {
@@ -79,6 +111,26 @@ export default function Settings() {
             [name]: value,
         });
     };
+
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setDrugInputs({
+            ...DrugInputs,
+            [name]: value,
+        });
+        setShowToast(false)
+    };
+    const handleDrugSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault(); // Prevent default behavior of the button if needed.
+
+        // Example form validation: check if required fields are filled
+        if (!DrugInputs.GSTnumber && !DrugInputs.PANnumber) {
+            setShowToast(true);
+        }
+
+    };
+
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -106,7 +158,6 @@ export default function Settings() {
         }
         setShowToast(false);
 
-        // Trigger validation feedback
     };
     return (
         <div className="Settings-section">
@@ -130,14 +181,14 @@ export default function Settings() {
                                                             <div className="settings-img">
                                                                 <img src={img} alt="" ref={imgPreviewRef} />
                                                                 <form action="">
-                                                                <input
-                                                                    type="file"
-                                                                    ref={fileInputRef}
-                                                                    onChange={validateFileType}
-                                                                    style={{ display: 'none' }}
-                                                                />
+                                                                    <input
+                                                                        type="file"
+                                                                        ref={fileInputRef}
+                                                                        onChange={validateFileType}
+                                                                        style={{ display: 'none' }}
+                                                                    />
                                                                 </form>
-                                                               
+
                                                                 <Button onClick={() => fileInputRef.current?.click()} className='change-Img'>Change Image</Button>
                                                             </div>
 
@@ -399,7 +450,118 @@ export default function Settings() {
                                         </div>
                                     </Tab>
                                     <Tab eventKey="Drug Permits" title="Drug Permits">
-                                        Tab content for Profile
+                                        <Row className='mt-4'>
+
+                                            <Card >
+                                                <Card.Body>
+                                                    <div className="card-box ">
+                                                        <div className="cards">
+                                                            <div className="card-border">
+                                                                <div onClick={() => form20Ref.current?.click()} className="image-preview text-gray-700 border-dashed">
+                                                                    <img
+                                                                        className="preview-img"
+                                                                        src={form20Preview || ''}
+                                                                        alt="Form 20 Preview"
+                                                                        style={{ display: form20Preview ? 'block' : 'none' }}
+                                                                    />
+                                                                    <span  style={{ display: form20Preview ? 'none' : 'block' }} className="mr-2">Upload Form 20</span>
+                                                                    <i className="fas fa-upload"></i>
+                                                                    <input
+                                                                        type="file"
+                                                                        ref={form20Ref}
+                                                                        accept=".jpg,.jpeg,.png,.pdf"
+                                                                        style={{ display: 'none' }}
+                                                                        onChange={(e) => handleFileChange(e, 'form20')}
+                                                                    />
+                                                                   
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="cards">
+                                                            <div className="card-border">
+                                                                <div id="imagePreviewForm20" className="image-preview text-gray-700 border-dashed">
+                                                                    <span className="mr-2">Upload Form 21</span>
+                                                                    <i className="fas fa-upload"></i>
+                                                                </div>
+
+                                                            </div>
+
+
+                                                        </div>
+                                                    </div>
+                                                </Card.Body>
+                                            </Card>
+
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <div className="inputs">
+                                                    <Form id="drugPermitsForm" className="inputs">
+                                                        <div className="form-group inputwidth">
+                                                            <div className="floating-label">
+                                                                <FormControl
+                                                                    type="text"
+                                                                    className="form-control"
+                                                                    id="GSTnumber"
+                                                                    name="GSTnumber"
+                                                                    value={DrugInputs.GSTnumber}
+                                                                    onChange={handleInputChange}
+                                                                    maxLength={15}
+                                                                    minLength={15}
+                                                                    isInvalid={!GstNumberRegex.test(DrugInputs.GSTnumber)}
+                                                                    placeholder=""
+                                                                    required
+                                                                />
+                                                                <label htmlFor="OrganizationName">
+                                                                    Enter Organization GST number <span style={{ color: 'red' }}>*</span>
+                                                                </label>
+                                                                <Form.Control.Feedback type="invalid">
+                                                                    Please enter a valid  GST number.
+                                                                </Form.Control.Feedback>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="form-group">
+                                                            <div className="floating-label inputwidth">
+                                                                <FormControl
+                                                                    type="text"
+                                                                    className="form-control"
+                                                                    id="PANnumber"
+                                                                    name="PANnumber"
+                                                                    value={DrugInputs.PANnumber}
+                                                                    onChange={handleInputChange}
+                                                                    maxLength={10}
+                                                                    minLength={10}
+                                                                    isInvalid={!GstNumberRegex.test(DrugInputs.PANnumber)}
+                                                                    placeholder=""
+                                                                    required
+                                                                />
+                                                                <label htmlFor="OrganizationName">
+                                                                    Enter PAN number <span style={{ color: 'red' }}>*</span>
+                                                                </label>
+                                                                <Form.Control.Feedback type="invalid">
+                                                                    Please enter a valid  PAN number.
+                                                                </Form.Control.Feedback>
+                                                            </div>
+                                                        </div>
+
+
+
+
+                                                    </Form>
+
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                        <div className="btns d-flex justify-content-end px-3">
+                                            <Button onClick={handleDrugSubmit}>Submit</Button>
+                                        </div>
+
+
+
+
+
                                     </Tab>
                                     <Tab eventKey="Subscriptions" title="Subscriptions">
                                         <div className="Subscriptions">
